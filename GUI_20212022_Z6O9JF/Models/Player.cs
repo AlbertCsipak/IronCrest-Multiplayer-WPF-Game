493cd3dba5c2d3c05fb9605c4bf80e90fdcc6091
csipakalbert <csipakalbert0@gmail.com>
2022-03-28 00:35:46 +0200

    Folders

---
+++ GUI_20212022_Z6O9JF/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212022_Z6O9JF.Models
+{
+    class Player
+    {
+    }
+}
