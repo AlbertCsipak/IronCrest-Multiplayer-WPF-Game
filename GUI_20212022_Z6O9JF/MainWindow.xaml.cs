01e086c5fb7d1f0f2c96c167596c5c4abbd7f27b
HLi01 <hermanl@stud.uni-obuda.hu>
2022-04-01 21:57:44 +0200

    trying out sounds

--- GUI_20212022_Z6O9JF/MainWindow.xaml.cs
+++ GUI_20212022_Z6O9JF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GUI_20212022_Z6O9JF.ViewModels;
 using System;
+using System.Media;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
@@ -11,50 +13,43 @@ namespace GUI_20212022_Z6O9JF
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        public MediaElement background_music;
+        public MediaElement button_clink_sound;
+        SoundPlayer player = new SoundPlayer("file.wav");
         public Uri unmutedUri = new Uri("Resources/Images/Other/unmuted.png", UriKind.RelativeOrAbsolute);
         public Uri mutedUri = new Uri("Resources/Images/Other/muted.png", UriKind.RelativeOrAbsolute);
         Cursor c1;
+
         public MainWindow()
         {
-
+            background_music = new MediaElement();
+            button_clink_sound = new MediaElement();
             InitializeComponent();
             c1 = new Cursor("Resources/blurite_sword.cur");
             grid.Cursor = c1;
             this.DataContext = new MainViewModel();
             img_mute.Source = new BitmapImage(unmutedUri);
-            music.Play();
+            
+            button_clink_sound.Source = new Uri("Resources/Music/button.mp3", UriKind.RelativeOrAbsolute);
+            
+            background_music.Source = new Uri("Resources/Music/standard.mp3", UriKind.RelativeOrAbsolute);
+            background_music.LoadedBehavior = MediaState.Manual;
+            button_clink_sound.LoadedBehavior = MediaState.Manual;
+            background_music.Play();
 
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            if (music.IsMuted)
-            {
-                music.IsMuted = false;
-                music.Play();
-                img_mute.Source = new BitmapImage(mutedUri);
-            }
-            else
-            {
-                music.IsMuted = true;
-                music.Pause();
-                img_mute.Source = new BitmapImage(unmutedUri);
-            }
         }
 
         private void volume_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             double volume = volume_slider.Value;
-            music.Volume = volume / 100;
+            background_music.Volume = volume / 100;
             if (volume == 0.0)
             {
-                music.IsMuted = true; img_mute.Source = new BitmapImage(mutedUri);
+                background_music.IsMuted = true; img_mute.Source = new BitmapImage(mutedUri);
             }
             else
             {
-                music.IsMuted = false; img_mute.Source = new BitmapImage(unmutedUri);
+                background_music.IsMuted = false; img_mute.Source = new BitmapImage(unmutedUri);
             }
         }
     }
--- GUI_20212022_Z6O9JF/UserControls/MenuUC.xaml.cs
+++ GUI_20212022_Z6O9JF/UserControls/MenuUC.xaml.cs
@@ -19,6 +19,12 @@ namespace GUI_20212022_Z6O9JF.UserControls
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            
+            
+        }
+
+        private void Button_Exit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
             System.Windows.Application.Current.Shutdown();
         }
     }
