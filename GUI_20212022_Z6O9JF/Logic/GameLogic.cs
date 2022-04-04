4d14a3c411621e7a5dfe7a94bf1e1051fab3b15a
csipakalbert <csipakalbert0@gmail.com>
2022-04-05 01:07:10 +0200

    CodeCleanup

--- GUI_20212022_Z6O9JF/Logic/GameLogic.cs
+++ GUI_20212022_Z6O9JF/Logic/GameLogic.cs
@@ -3,7 +3,6 @@ using Microsoft.Toolkit.Mvvm.Messaging;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Windows;
 
 namespace GUI_20212022_Z6O9JF.Logic
 {
