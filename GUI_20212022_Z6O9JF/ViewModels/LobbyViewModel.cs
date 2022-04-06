e76d1c50295edfb96d6a89880b4d2e30cbc73082
csipakalbert <csipakalbert0@gmail.com>
2022-04-06 22:21:21 +0200

    Lobby Index Error Fix

--- GUI_20212022_Z6O9JF/ViewModels/LobbyViewModel.cs
+++ GUI_20212022_Z6O9JF/ViewModels/LobbyViewModel.cs
@@ -41,7 +41,7 @@ namespace GUI_20212022_Z6O9JF.ViewModels
 
             Name = "Anon";
 
-            GameCommand = new RelayCommand(() => clientLogic.ChampSelect(SelectedFaction, Name));
+            GameCommand = new RelayCommand(() => { clientLogic.ChampSelect(SelectedFaction, Name); index = 0; });
             NextFaction = new RelayCommand(() =>
             {
                 if (gameLogic.AvailableFactions.Count - 1 > index)
