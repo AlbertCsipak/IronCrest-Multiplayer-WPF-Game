51b041756f42d5b16174f7fea186a28041383ede
csipakalbert <csipakalbert0@gmail.com>
2022-04-11 20:30:36 +0200

    DataLossFix2

--- GUI_20212022_Z6O9JF/Logic/ClientLogic.cs
+++ GUI_20212022_Z6O9JF/Logic/ClientLogic.cs
@@ -81,7 +81,7 @@ namespace GUI_20212022_Z6O9JF.Logic
                             {
                                 Timer = 60;
                             }
-                            else if (!message.Contains("[]"))
+                            else if (message.Length>3)
                             {
                                 Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => gameLogic.Players.Clear()));
                                 try
