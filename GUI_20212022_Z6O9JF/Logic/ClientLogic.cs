7667653310300fe29978f1685972c739b5eecbcf
HLi01 <hermanl@stud.uni-obuda.hu>
2022-04-11 23:15:40 +0200

    ClientConnect method edited

--- GUI_20212022_Z6O9JF/Logic/ClientLogic.cs
+++ GUI_20212022_Z6O9JF/Logic/ClientLogic.cs
@@ -82,7 +82,7 @@ namespace GUI_20212022_Z6O9JF.Logic
                             {
                                 Timer = 60;
                             }
-                            else
+                            else if(!message.Equals(""))
                             {
                                 try
                                 {
