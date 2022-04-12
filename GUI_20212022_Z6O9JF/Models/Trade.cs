85ccbe3c93d6bc2ff1405f7235f10680bcf75d66
HLi01 <hermanl@stud.uni-obuda.hu>
2022-04-12 11:56:59 +0200

    trade class created

---
+++ GUI_20212022_Z6O9JF/Models/Trade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212022_Z6O9JF.Models
+{
+    public class Trade
+    {
+
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string Text3 { get; set; }
+    }
+}
