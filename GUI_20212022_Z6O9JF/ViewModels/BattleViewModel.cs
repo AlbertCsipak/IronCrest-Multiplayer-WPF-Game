5b2e0c6168d8aca6c5927b2d3084884fd47158df
HLi01 <hermanl@stud.uni-obuda.hu>
2022-04-19 12:14:57 +0200

    walking character gifs added

--- GUI_20212022_Z6O9JF/ViewModels/BattleViewModel.cs
+++ GUI_20212022_Z6O9JF/ViewModels/BattleViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using GUI_20212022_Z6O9JF.Logic;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GUI_20212022_Z6O9JF.ViewModels
 {
-    class BattleViewModel
+    public class BattleViewModel : ObservableRecipient
     {
+        public IGameLogic gameLogic { get; set; }
+        public IClientLogic clientLogic { get; set; }
+        public IControlLogic controlLogic { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                return
+                    (bool)DependencyPropertyDescriptor
+                    .FromProperty(DesignerProperties.
+                    IsInDesignModeProperty,
+                    typeof(FrameworkElement))
+                    .Metadata.DefaultValue;
+            }
+        }
+        public BattleViewModel() : this(IsInDesignMode ? null : Ioc.Default.GetService<IGameLogic>(), Ioc.Default.GetService<IClientLogic>(), Ioc.Default.GetService<IControlLogic>()) { }
+        public BattleViewModel(IGameLogic gameLogic, IClientLogic clientLogic, IControlLogic controlLogic)
+        {
+            this.controlLogic = controlLogic;
+            this.gameLogic = gameLogic;
+            this.clientLogic = clientLogic;
+        }
     }
 }
