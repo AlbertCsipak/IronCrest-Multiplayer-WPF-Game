a21b26d327a09596d528e3ad9db561a4e5d3f670
HLi01 <hermanl@stud.uni-obuda.hu>
2022-04-20 22:26:03 +0200

    mysteryHero UC and viewmodel created

---
+++ GUI_20212022_Z6O9JF/UserControls/MysteryHeroUC.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GUI_20212022_Z6O9JF.Logic;
+using GUI_20212022_Z6O9JF.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GUI_20212022_Z6O9JF.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MysteryUC.xaml
+    /// </summary>
+    public partial class MysteryHeroUC : UserControl
+    {
+        IClientLogic clientLogic;
+        IGameLogic gameLogic;
+        IControlLogic controlLogic;
+        public MediaPlayer button_click = new MediaPlayer();
+        public MysteryHeroUC()
+        {
+            InitializeComponent();
+            this.DataContext = new MysteryHeroViewModel();
+            this.gameLogic = (this.DataContext as MysteryHeroViewModel).gameLogic;
+            this.clientLogic = (this.DataContext as MysteryHeroViewModel).clientLogic;
+            this.controlLogic = (this.DataContext as MysteryHeroViewModel).controlLogic;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //gameLogic.Players.Where(x => x.PlayerID == clientLogic.ClientId).FirstOrDefault().
+        }
+    }
+}
---
+++ GUI_20212022_Z6O9JF/ViewModels/MysteryHeroViewModel.cs
@@ -0,0 +1,51 @@
+﻿using GUI_20212022_Z6O9JF.Logic;
+using GUI_20212022_Z6O9JF.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GUI_20212022_Z6O9JF.ViewModels
+{
+    public class MysteryHeroViewModel : ObservableRecipient
+    {
+        public IGameLogic gameLogic { get; set; }
+        public IClientLogic clientLogic { get; set; }
+        public IControlLogic controlLogic { get; set; }
+        public ICommand OKButtonCommand { get; set; }
+        public string MysteryEventDescription { get { return gameLogic.CurrentMystery.Description; } }
+        public FieldType Background { get { return gameLogic.CurrentMystery.FieldType; } }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                return
+                    (bool)DependencyPropertyDescriptor
+                    .FromProperty(DesignerProperties.
+                    IsInDesignModeProperty,
+                    typeof(FrameworkElement))
+                    .Metadata.DefaultValue;
+            }
+        }
+        public MysteryHeroViewModel() : this(IsInDesignMode ? null : Ioc.Default.GetService<IGameLogic>(), Ioc.Default.GetService<IClientLogic>(), Ioc.Default.GetService<IControlLogic>()) { }
+        public MysteryHeroViewModel(IGameLogic gameLogic, IClientLogic clientLogic, IControlLogic controlLogic)
+        {
+            this.controlLogic = controlLogic;
+            this.gameLogic = gameLogic;
+            this.clientLogic = clientLogic;
+
+            OKButtonCommand = new RelayCommand(() =>
+            {
+                clientLogic.MysteryButtonOK();
+                clientLogic.TradeViewChange("asd");
+            });
+
+            Messenger.Register<MysteryHeroViewModel, string, string>(this, "Base", (recipient, msg) =>
+            {
+                OnPropertyChanged("CanSend");
+            });
+        }
+    }
+}
