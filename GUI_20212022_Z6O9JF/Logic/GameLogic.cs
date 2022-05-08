dc681565db5a4e2167d089d14f37ae54c1725455
HLi01 <hermanl@stud.uni-obuda.hu>
2022-05-08 17:39:24 +0200

    fix

--- GUI_20212022_Z6O9JF/Logic/GameLogic.cs
+++ GUI_20212022_Z6O9JF/Logic/GameLogic.cs
@@ -200,7 +200,7 @@ namespace GUI_20212022_Z6O9JF.Logic
             Shuffle(trades);
             Queue<Trade> tradeQueue = new Queue<Trade>();
             trades.ForEach(x => tradeQueue.Enqueue(x));
-            Game.Trades = trades;
+            //Game.Trades = trades;
             return tradeQueue;
         }
 
